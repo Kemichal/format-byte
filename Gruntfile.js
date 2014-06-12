92723d86906c6d483e34757f801288d1ecc00c5f
Kemichal <kemichal@gmail.com>
2014-06-12 07:07:35 +0200

    Scaffolding

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+	require('load-grunt-tasks')(grunt);
+
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		mochaTest: {
+			test: {
+				options: {
+					reporter: 'spec'
+				},
+				src: ['test/*.js']	
+			}
+		}
+	});
+
+	grunt.registerTask('test', ['mochaTest']);
+};
